cb155e1177fba3cabb9d280d50ace94a60304dd7
Tomass <tomassaulite@gmail.com>
2026-07-25 15:01:00 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-app-bar color="primary" density="comfortable">
+      <v-app-bar-title>FridgeRaid</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container class="py-12">
+        <v-card class="mx-auto pa-6" max-width="600">
+          <v-card-title class="text-h4">Vue + Vuetify is ready</v-card-title>
+          <v-card-text>
+            Start building your FridgeRaid experience with Vue and Vuetify.
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary" prepend-icon="mdi-rocket-launch">Get started</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import './style.css'
+import App from './App.vue'
+
+const vuetify = createVuetify({
+  icons: { defaultSet: 'mdi' },
+})
+
+createApp(App).use(vuetify).mount('#app')
